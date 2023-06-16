1d3c6b5cb70eb74c624abb854516ed8684d38851
Ricardoxt1 <ricardoalexandr_@hotmail.com>
2023-06-16 09:48:25 -0300

    atualização: ordem das variaveis

--- php_app/books/register/register_books.php
+++ php_app/books/register/register_books.php
@@ -8,12 +8,12 @@ $tabela = 'books';
 // Inserting authors into the database
 $smt = $pdo->prepare('INSERT INTO ' . $tabela . ' (titule, page, realese_date, author_id) VALUES (:titule, :page, :realese_date, :author_id)');
 $smt->bindParam(':titule', $titule);
-$titule = 'branca de neve';
 $smt->bindParam(':page', $page);
-$page = '500';
 $smt->bindParam(':realese_date', $realese_date);
-$realese_date = '2001';
 $smt->bindParam(':author_id', $author_id);
+$titule = 'branca de neve';
+$page = '500';
+$realese_date = '2001';
 $author_id = '2';
 
 $smt->execute();
