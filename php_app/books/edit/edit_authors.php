c185542bcaf3c74fcbdca3fc1ee5c8f46df61872
Ricardoxt1 <ricardoalexandr_@hotmail.com>
2023-06-16 17:13:56 -0300

    edit: criação da funcionalidade para authors

---
+++ php_app/books/edit/edit_authors.php
@@ -0,0 +1,13 @@
+<?php
+include_once('../../config.php');
+$pdo = conectar();
+
+$id = 1;
+$nome = "Sarah J Maas";
+$tabela = 'authors';
+
+$stmt = $pdo->prepare('UPDATE ' . $tabela . ' SET name = :name WHERE id = :id');
+$stmt->execute(array(
+    ':id' => $id,
+    ':name' => $nome
+));
