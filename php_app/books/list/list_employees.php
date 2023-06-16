956f1befaa4d5f9d012badd2c9b8a621f0c0b92d
Ricardoxt1 <ricardoalexandr_@hotmail.com>
2023-06-16 12:09:37 -0300

    list: elaboração da chamada de funcionários

---
+++ php_app/books/list/list_employees.php
@@ -0,0 +1,24 @@
+<?php
+
+include_once('../../config.php');
+$pdo = conectar();
+
+$tabela = 'employees';
+
+//busca de dados sobre os livros
+
+$id = 1;
+$stmt = $pdo->prepare('SELECT * FROM ' . $tabela . ' WHERE id = :id');
+$stmt->bindValue(':id', $id);
+$stmt->execute();
+
+$resultado = $stmt->fetchAll();
+
+
+foreach ($resultado as $key) {
+    print_r('Nome: ' . $key['name']);
+    print_r('<hr>PIS: ' . $key['pis']);
+    print_r('<hr>Cargo: ' . $key['office']);
+    print_r('<hr>Setor: ' . $key['departament']);
+    print_r('<hr>Empresa: ' . $key['library_id']);
+}
