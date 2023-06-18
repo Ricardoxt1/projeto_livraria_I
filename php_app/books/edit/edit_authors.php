a6ec28bc63b74719635cd5d9b535e7aaa75ad3e3
Ricardoxt1 <ricardoalexandr_@hotmail.com>
2023-06-18 14:29:40 -0300

    modificação

--- php_app/books/edit/edit_authors.php
+++ php_app/books/edit/edit_authors.php
@@ -3,11 +3,11 @@ include_once('../../config.php');
 $pdo = conectar();
 
 $id = 1;
-$nome = "Sarah J Maas";
+$name = "Sarah J Maas";
 $tabela = 'authors';
 
 $stmt = $pdo->prepare('UPDATE ' . $tabela . ' SET name = :name WHERE id = :id');
 $stmt->execute(array(
     ':id' => $id,
-    ':name' => $nome
+    ':name' => $name
 ));
