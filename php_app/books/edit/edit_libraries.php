a6cdaccc30a12201097baf5ec894ad3c2d21e6bd
Ricardoxt1 <ricardoalexandr_@hotmail.com>
2023-06-18 14:31:48 -0300

    edit: funcionalidade adicionada para editar libraries

---
+++ php_app/books/edit/edit_libraries.php
@@ -0,0 +1,14 @@
+<?php
+include_once('../../config.php');
+$pdo = conectar();
+
+$id = 1;
+$name = "ffffffff";
+
+$tabela = 'libraries';
+
+$stmt = $pdo->prepare('UPDATE ' . $tabela . ' SET name = :name WHERE id = :id');
+$stmt->execute(array(
+    ':id' => $id,
+    ':name' => $name
+));
