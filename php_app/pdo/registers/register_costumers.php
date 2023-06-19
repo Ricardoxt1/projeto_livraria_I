da65a75f1ff8d52c3407de7941189e22968f1f54
Ricardoxt1 <ricardoalexandr_@hotmail.com>
2023-06-19 14:33:24 -0300

    opção de registrar email do usuario

--- php_app/pdo/registers/register_costumers.php
+++ php_app/pdo/registers/register_costumers.php
@@ -6,13 +6,15 @@ $pdo = conectar();
 $tabela = 'costumers';
 
 // Inserting costumers into the database
-$smt = $pdo->prepare('INSERT INTO ' . $tabela . ' (cpf, name, phone_number, address) VALUES (:cpf, :name, :phone_number, :address)');
+$smt = $pdo->prepare('INSERT INTO ' . $tabela . ' (cpf, name, phone_number, address, email) VALUES (:cpf, :name, :phone_number, :address, :email)');
 $smt->bindParam(':cpf', $cpf);
 $smt->bindParam(':name', $name);
 $smt->bindParam(':phone_number', $phone_number);
 $smt->bindParam(':address', $address);
-$cpf = '47583811864' ;
+$smt->bindParam(':email', $email);
+$cpf = '47583811864';
 $name = 'ricardo alexandre';
-$phone_number = '14998309985' ;
-$address = 'manoel ribeiro, 211' ;
+$phone_number = '14998309985';
+$address = 'manoel ribeiro, 211';
+$email = 'ricardo@gmail.com';
 $smt->execute();
