1b68dbe370bcee1c37b51d3ff7e8387e661ec0c1
Ricardoxt1 <ricardoalexandr_@hotmail.com>
2023-06-19 14:32:32 -0300

    acrescentando opção de edição para atributo email

--- php_app/pdo/edit/edit_costumers.php
+++ php_app/pdo/edit/edit_costumers.php
@@ -7,15 +7,17 @@ $cpf = "000.000.000.00";
 $name = "dasdddddddd";
 $phone_number = "00 00000-0000";
 $address = "rua numero 30";
+$email = "ricardo@gmail.com";
 
 $tabela = 'costumers';
 
-$stmt = $pdo->prepare('UPDATE ' . $tabela . ' SET cpf = :cpf, name = :name, phone_number = :phone_number, address = :address WHERE :id');
+$stmt = $pdo->prepare('UPDATE ' . $tabela . ' SET cpf = :cpf, name = :name, phone_number = :phone_number, address = :address, email = :email WHERE :id');
 $stmt->execute(array(
     ':id' => $id,
     ':cpf' => $cpf,
     ':name' => $name,
     ':phone_number' => $phone_number,
     ':address' => $address,
+    ':email' => $email,
 
 ));
