e73059f15ad60af0853d16a7c5988688af5bdebc
Ricardoxt1 <ricardoalexandr_@hotmail.com>
2023-06-21 09:02:27 -0300

    atualização do arquivo

---
+++ php_app/front/controllers/list/list_users.php
@@ -0,0 +1,199 @@
+<!doctype html>
+<html lang="pt">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Um projeto voltado ao sistema de gestão para biblioteca">
+    <meta name="Ricardo" content="Sistema de biblioteca">
+    <meta name="generator" content="Ricardo">
+    <title>Listagem de Usuarios</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/dashboard/">
+
+    <link href="../../bootstrap-5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            user-select: none;
+        }
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+
+        .b-example-divider {
+            height: 3rem;
+            background-color: rgba(0, 0, 0, .1);
+            border: solid rgba(0, 0, 0, .15);
+            border-width: 1px 0;
+            box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
+        }
+
+        .b-example-vr {
+            flex-shrink: 0;
+            width: 1.5rem;
+            height: 100vh;
+        }
+
+        .bi {
+            vertical-align: -.125em;
+            fill: currentColor;
+        }
+
+        .nav-scroller {
+            position: relative;
+            z-index: 2;
+            height: 2.75rem;
+            overflow-y: hidden;
+        }
+
+        .nav-scroller .nav {
+            display: flex;
+            flex-wrap: nowrap;
+            padding-bottom: 1rem;
+            margin-top: -1px;
+            overflow-x: auto;
+            text-align: center;
+            white-space: nowrap;
+            -webkit-overflow-scrolling: touch;
+        }
+    </style>
+
+
+    <!-- Custom styles for this template -->
+    <link href="../dashboard/dashboard.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <header class="navbar navbar-dark sticky-top bg-primary flex-md-nowrap p-0 shadow">
+        <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#">Biblioteca Pedbot</a>
+        <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="navbar-nav">
+            <div class="nav-item text-nowrap">
+                <a class="nav-link px-3" href="../../controllers/">Voltar ao menu</a>
+            </div>
+        </div>
+    </header>
+
+    <div class="container-fluid">
+        <div class="row">
+            <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+                <div class="position-sticky pt-3 sidebar-sticky">
+                    <ul class="nav flex-column">
+                        <li>
+                            <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
+                                <span>Listagem de itens</span>
+                                <a class="link-secondary" href="#" aria-label="Add a new report">
+                                    <span data-feather="plus-circle" class="align-text-bottom"></span>
+                                </a>
+                            </h6>
+
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="../list/list_books.php">
+                                <span data-feather="list_users" class="align-text-bottom"></span>
+                                Livros
+                            </a>
+
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="../controllers/list_authors.php">
+                                <span data-feather="list_users" class="align-text-bottom"></span>
+                                Autores
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="../controllers/list_costumers.php">
+                                <span data-feather="leitores" class="align-text-bottom"></span>
+                                Consumidores
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="../../pdo/registers/register_rentals.php">
+                                <span name="name" class="align-text-bottom"></span>
+                                Alugueis
+                            </a>
+                        </li>
+
+                    </ul>
+
+                    <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
+                        <span>Opções</span>
+                    </h6>
+                    <ul class="nav flex-column mb-2">
+                        <li class="nav-item">
+                            <a class="nav-link" href="../registers/register_costumers.php">
+                                <span data-feather="file-text" class="align-text-bottom"></span>
+                                Cadastrar
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="../controllers/edit_users.php">
+                                <span data-feather="file-text" class="align-text-bottom"></span>
+                                Editar itens
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
+            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                    <h1 class="h2">Listagem de usuarios</h1>
+                </div>
+
+                <!-- <h2>Livros</h2> -->
+                <div class="table-responsive">
+                    <table class="table table-striped table-sm">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Nome</th>
+                                <th scope="col">Telefone</th>
+                                <th scope="col">Endereço</th>
+                                <th scope="col">Email</th>
+                            </tr>
+                        </thead>
+                        <form action="../../../pdo/list/list_costumers.php" method="get">
+                            <tbody>
+                                <tr>
+                                    <td name="id_costumers">1</td>
+                                    <td name="name_costumers">nome</td>
+                                    <td name="phone_number_costumers">telefone</td>
+                                    <td name="address_costumers">endereço</td>
+                                    <td name="email_costumers">email</td>
+                                </tr>
+
+                            </tbody>
+                        </form>
+                    </table>
+                    <footer class="text-muted py-5">
+                        <div class="container">
+                            <p class="mb-1">© 2023 Biblioteca Pedbot</p>
+                        </div>
+                    </footer>
+                </div>
+            </main>
+        </div>
+    </div>
+
+
+    <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script>
+    <script src="dashboard.js"></script>
+</body>
+
+</html>
