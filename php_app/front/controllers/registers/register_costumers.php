19054820ebe06cbd27d8b794e39cc8630a6c5f74
Ricardoxt1 <ricardoalexandr_@hotmail.com>
2023-06-21 12:16:26 -0300

    crianção conexão front - back para usuarios

--- php_app/front/controllers/registers/register_costumers.php
+++ php_app/front/controllers/registers/register_costumers.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!doctype html>
 <html lang="pt">
 
@@ -75,9 +78,7 @@
 
     <header class="navbar navbar-dark sticky-top bg-primary flex-md-nowrap p-0 shadow">
         <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#">Biblioteca Pedbot</a>
-        <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse"
-            data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false"
-            aria-label="Toggle navigation">
+        <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
@@ -94,8 +95,7 @@
                 <div class="position-sticky pt-3 sidebar-sticky">
                     <ul class="nav flex-column">
                         <li class="nav-item">
-                            <h6
-                                class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
+                            <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
                                 <span>Cadastro</span>
                             </h6>
 
@@ -128,8 +128,7 @@
 
                     </ul>
 
-                    <h6
-                        class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
+                    <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
                         <span>Opções</span>
                         <a class="link-secondary" href="#" aria-label="Add a new report">
                             <span data-feather="plus-circle" class="align-text-bottom"></span>
@@ -143,13 +142,13 @@
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="../list/list_users.html">
+                            <a class="nav-link" href="../list/list_users.php">
                                 <span data-feather="file-text" class="align-text-bottom"></span>
                                 Listagem
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="../edit/edit_users.html">
+                            <a class="nav-link" href="../edit/edit_users.php">
                                 <span data-feather="file-text" class="align-text-bottom"></span>
                                 Editar itens
                             </a>
@@ -159,8 +158,7 @@
             </nav>
 
             <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-                <div
-                    class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
 
                     <body class="bg-body-tertiary">
 
@@ -169,20 +167,21 @@
                                 <div class="py-5 text-center">
                                     <h2>Cadastro de Usuarios</h2>
                                 </div>
-
+                                <?php
+                                if (isset($_SESSION['msg'])) {
+                                    echo $_SESSION['msg'];
+                                    unset($_SESSION['msg']);
+                                }
+                                ?>
                                 <div class="row g-5">
 
                                     <div class="col-md-7 col-lg-12">
                                         <h4 class="mb-3">Registro dados pessoais</h4>
-                                        <form class="needs-validation"
-                                            action="../../../pdo/registers/register_costumers.php" method="post"
-                                            novalidate="">
+                                        <form class="needs-validation" action="../../../pdo/registers/register_costumers.php" method="post" novalidate="">
                                             <div class="row g-3">
                                                 <div class="col-sm-7">
                                                     <label for="nome_costumer" class="form-label">Nome completo</label>
-                                                    <input type="text" class="form-control" name="name"
-                                                        id="nome_costumer" placeholder="fulano da silva " value=""
-                                                        required="">
+                                                    <input type="text" class="form-control" name="name" id="nome_costumer" placeholder="fulano da silva " value="" required="">
                                                     <div class="invalid-feedback">
                                                         É necessario digitar o seu nome.
                                                     </div>
@@ -190,18 +189,14 @@
 
                                                 <div class="col-sm-5">
                                                     <label for="numero_costumer" class="form-label">Telefone</label>
-                                                    <input type="number" class="form-control" name="phone_number"
-                                                        id="numero_costumer" placeholder="(00) 0000-0000" value=""
-                                                        required="">
+                                                    <input type="number" class="form-control" name="phone_number" id="numero_costumer" placeholder="(00) 0000-0000" value="" required="">
                                                     <div class="invalid-feedback">
                                                         É necessario digitar o seu telefone.
                                                     </div>
                                                 </div>
                                                 <div class="col-sm-7">
-                                                    <label for="email_costumer" class="form-label">Email <span
-                                                            class="text-body-secondary">(Optional)</span></label>
-                                                    <input type="email" class="form-control" name="email"
-                                                        id="email_costumer" placeholder="you@example.com">
+                                                    <label for="email_costumer" class="form-label">Email <span class="text-body-secondary">(Optional)</span></label>
+                                                    <input type="email" class="form-control" name="email" id="email_costumer" placeholder="you@example.com">
                                                     <div class="invalid-feedback">
                                                         Por favor, entre com um email valido.
                                                     </div>
@@ -209,9 +204,7 @@
 
                                                 <div class="col-sm-5">
                                                     <label for="cpf_costumer" class="form-label">CPF</label>
-                                                    <input type="number" class="form-control" name="cpf"
-                                                        id="cpf_costumer" placeholder="123.456.789-09" max="14" value=""
-                                                        required="">
+                                                    <input type="number" class="form-control" name="cpf" id="cpf_costumer" placeholder="123.456.789-09" min="1" max="14" value="" required="">
                                                     <div class="invalid-feedback">
                                                         É necessario digitar o cpf.
                                                     </div>
@@ -219,8 +212,7 @@
 
                                                 <div class="col-12">
                                                     <label for="address_costumer" class="form-label">Endereço</label>
-                                                    <input type="text" class="form-control" name="address"
-                                                        id="address_costumer" placeholder="Main ST 1234" required="">
+                                                    <input type="text" class="form-control" name="address" id="address_costumer" placeholder="Main ST 1234" required="">
                                                     <div class="invalid-feedback">
                                                         Por favor, entre com endereço valido.
                                                     </div>
@@ -252,12 +244,8 @@
 
     <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
 
-    <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js"
-        integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js"
-        integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha"
-        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script>
     <script src="dashboard.js"></script>
 </body>
 
--- php_app/pdo/registers/register_costumers.php
+++ php_app/pdo/registers/register_costumers.php
@@ -1,20 +1,41 @@
 <?php
-
+session_start();
 include_once('../../config.php');
 $pdo = conectar();
 
 $tabela = 'costumers';
 
-// Inserting costumers into the database
-$smt = $pdo->prepare('INSERT INTO ' . $tabela . ' (cpf, name, phone_number, address, email) VALUES (:cpf, :name, :phone_number, :address, :email)');
-$smt->bindParam(':cpf', $cpf);
-$smt->bindParam(':name', $name);
-$smt->bindParam(':phone_number', $phone_number);
-$smt->bindParam(':address', $address);
-$smt->bindParam(':email', $email);
-$cpf = '47583811864';
-$name = 'ricardo alexandre';
-$phone_number = '14998309985';
-$address = 'manoel ribeiro, 211';
-$email = 'ricardo@gmail.com';
-$smt->execute();
+try {
+    $tabela = 'costumers';
+
+    // Inserting costumers into the database
+    $smt = $pdo->prepare('INSERT INTO ' . $tabela . ' (cpf, name, phone_number, address, email) VALUES (:cpf, :name, :phone_number, :address, :email)');
+    $smt->bindParam(':cpf', $cpf);
+    $smt->bindParam(':name', $name);
+    $smt->bindParam(':phone_number', $phone_number);
+    $smt->bindParam(':address', $address);
+    $smt->bindParam(':email', $email);
+    $name = $_POST['name'] ? $_POST['name'] : false;
+    $phone_number = $_POST['phone_number'] ? $_POST['phone_number'] : false;
+    $email = $_POST['email'] ? $_POST['email'] : false;
+    $cpf = $_POST['cpf'] ? $_POST['cpf'] : false;
+    $address = $_POST['address'] ? $_POST['address'] : false;
+    $smt->execute();
+
+
+    $lastInsertId = $pdo->lastInsertId();
+
+    if ($lastInsertId) {
+        session_start();
+        $_SESSION['msg'] = "<p style='color:green;'>Usuário cadastrado com sucesso!</p>";
+        header("Location: /front/controllers/registers/register_costumers.php");
+        exit;
+    } else {
+        session_start();
+        $_SESSION['msg'] = "<p style='color:red;'>Cadastro não foi realizado com sucesso.</p>";
+        header("Location: /front/controllers/registers/register_costumers.php");
+        exit;
+    }
+} catch (PDOException $e) {
+    echo 'Error: ' . $e->getMessage();
+}
