9cf60f86b41b40c0c4e8c413aa931b39764034a2
Ricardoxt1 <ricardoalexandr_@hotmail.com>
2023-06-23 16:18:51 -0300

    atualizações visualização sidebar

--- php_app/front/controllers/list/list_books.php
+++ php_app/front/controllers/list/list_books.php
@@ -93,9 +93,6 @@ $pdo = conectar();
     <header>
         <header class="navbar navbar-dark sticky-top bg-primary flex-md-nowrap p-0 shadow">
             <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#">Biblioteca Pedbot</a>
-            <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
             <a href="https://getbootstrap.com/docs/5.2/examples/album/#" class="navbar-brand d-flex align-items-center">
 
                 <svg xmlns="http://www.w3.org/2000/svg" width="50" height="30" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" aria-hidden="true" class="me-2" viewBox="0 0 24 24">
@@ -171,8 +168,10 @@ $pdo = conectar();
             </div>
         </footer>
     </main>
-    <script src="./Album example · Bootstrap v5.2_files/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-
+    <script src="../../../bootstrap-5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script>
+    <script src="dashboard.js"></script>
 </body>
 
 </html>
--- php_app/front/controllers/list/list_employees.php
+++ php_app/front/controllers/list/list_employees.php
@@ -79,7 +79,7 @@ $pdo = conectar();
 
     <header class="navbar navbar-dark sticky-top bg-primary flex-md-nowrap p-0 shadow">
         <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6">Biblioteca Pedbot</a>
-        <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler position-center d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
@@ -131,19 +131,14 @@ $pdo = conectar();
                     <ul class="nav flex-column mb-2">
                         <li class="nav-item">
                             <a class="nav-link" href="../list/list_rentals.php">
-                                <span name="name" class="align-text-bottom">Alugueis</span>
+                                <span name="rentals" class="align-text-bottom">Alugueis</span>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="../registers/register_costumers.php">
-                                <span data-feather="file-text" class="align-text-bottom">Cadastrar</span>
+                                <span data-feather="registers" class="align-text-bottom">Cadastrar</span>
                             </a>
                         </li>
-                        <!-- <li class="nav-item">
-                            <a class="nav-link" href="../edit/edit_costumers.php">
-                                <span data-feather="file-text" class="align-text-bottom">Editar itens</span>
-                            </a>
-                        </li> -->
                     </ul>
                 </div>
             </nav>
@@ -218,9 +213,7 @@ $pdo = conectar();
         </div>
     </div>
 
-
-    <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
-
+    <script src="../../../bootstrap-5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script>
     <script src="dashboard.js"></script>
