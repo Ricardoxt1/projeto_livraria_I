e767f3f34a2c2883948c1d52b9df11e0116f8fb2
Ricardoxt1 <ricardoalexandr_@hotmail.com>
2023-06-26 17:48:40 -0300

    desenvolvendo funcionalidade edit

--- php_app/pdo/edit/edit_authors.php
+++ php_app/pdo/edit/edit_authors.php
@@ -1,13 +1,31 @@
 <?php
 include_once('../../config.php');
+include_once('../../front/controllers/edit/edit_authors.php');
 $pdo = conectar();
 
-$id = 1;
-$name = "Sarah J Maas";
-$tabela = 'authors';
+try {
 
-$stmt = $pdo->prepare('UPDATE ' . $tabela . ' SET name = :name WHERE id = :id');
-$stmt->execute(array(
-    ':id' => $id,
-    ':name' => $name
-));
+
+    $tabela = 'authors';
+    $stmt = $pdo->prepare("UPDATE $tabela SET name = :name WHERE id = :id");
+    $stmt->execute(array(
+        ':id' => $dados['id'],
+        ':name' => $dados['name'],
+    ));
+
+    $lastInsertId = $pdo->lastInsertId();
+
+    if ($lastInsertId) {
+        session_start();
+        $_SESSION['msg'] = "<p style='color:green;'>Autor editado com sucesso!</p>";
+        header("Location: /front/controllers/edit/edit_authors.php");
+        exit;
+    } else {
+        session_start();
+        $_SESSION['msg'] = "<p style='color:red;'>Editagem não foi realizado!.</p>";
+        header("Location: /front/controllers/edit/edit_authors.php");
+        exit;
+    }
+} catch (PDOException $e) {
+    echo 'Error: ' . $e->getMessage();
+}
