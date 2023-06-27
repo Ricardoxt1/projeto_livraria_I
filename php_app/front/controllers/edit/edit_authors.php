738d2a5d3f9b1ceb17859960c308c2c9547b6556
Ricardoxt1 <ricardoalexandr_@hotmail.com>
2023-06-27 16:10:24 -0300

    corrindo o problema de mostragem dos dados para editagem

--- php_app/front/controllers/edit/edit_authors.php
+++ php_app/front/controllers/edit/edit_authors.php
@@ -6,15 +6,10 @@ $pdo = conectar();
 
 $id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_NUMBER_INT);
 
-if (empty($id)) {
-    $_SESSION['msg'] = "<p style='color: #f00;'>Erro: Autor não encontrado!</p>";
-    header("Location: /front/controllers/list/list_authors.php");
-    exit;
-}
-
 $query_authors = "SELECT id, name FROM authors WHERE id = $id";
 $result_authors = $pdo->prepare($query_authors);
 $result_authors->execute();
+
 
 if (($result_authors) and ($result_authors->rowCount() != 0)) {
     $row_authors = $result_authors->fetch(PDO::FETCH_ASSOC);
@@ -29,9 +24,11 @@ if (($result_authors) and ($result_authors->rowCount() != 0)) {
 <html lang="pt">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Um projeto voltado ao sistema de gestão para biblioteca">
+    <meta name="Ricardo" content="Sistema de biblioteca">
+    <meta name="generator" content="Ricardo">
     <title>Edição de autores</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/dashboard/">
@@ -99,7 +96,7 @@ if (($result_authors) and ($result_authors->rowCount() != 0)) {
 <body>
     <header class="navbar navbar-dark sticky-top bg-primary flex-md-nowrap p-0 shadow">
         <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#">Biblioteca Pedbot</a>
-        <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler position-center d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
@@ -117,31 +114,30 @@ if (($result_authors) and ($result_authors->rowCount() != 0)) {
                     <ul class="nav flex-column">
                         <li>
                             <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
-                                <span>Cadastro</span>
+                                <span>Editar</span>
                             </h6>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="../../controllers/registers/register_costumers.php">
-                                <span data-feather="usuario" class="align-text-bottom"></span>
-                                Usuarios
+                            <a class="nav-link" href="../../controllers/list/list_costumers.php">
+                                <span data-feather="edit_costumers" class="align-text-bottom">Usuarios</span>
+
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="../../controllers/registers/register_books.php">
-                                <span data-feather="Livros" class="align-text-bottom"></span>
-                                Livros
+                            <a class="nav-link" href="../../controllers/list/list_books.php">
+                                <span data-feather="edit_books" class="align-text-bottom">Livros</span>
+
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="../../controllers/registers/register_publishers.php">
-                                <span data-feather="Editoras" class="align-text-bottom"></span>
-                                Editoras
+                            <a class="nav-link" href="../../controllers/list/list_publishers.php">
+                                <span data-feather="edit_publishers" class="align-text-bottom">Editoras</span>
+
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="../../controllers/registers/register_employees.php">
-                                <span data-feather="Funcionário(a)" class="align-text-bottom"></span>
-                                Funcionário(a)
+                            <a class="nav-link" href="../../controllers/list/list_employees.php">
+                                <span data-feather="edit_employees" class="align-text-bottom">Funcionário(a)</span>
                             </a>
                         </li>
 
@@ -150,21 +146,15 @@ if (($result_authors) and ($result_authors->rowCount() != 0)) {
                         </h6>
                         <ul class="nav flex-column mb-2">
                             <li class="nav-item">
-                                <a class="nav-link" href="../../controllers/registers/register_rentals.php">
-                                    <span name="name" class="align-text-bottom"></span>
-                                    Alugar livro
+                                <a class="nav-link" href="../../controllers/list/list_rentals.php">
+                                    <span name="edit_rentals" class="align-text-bottom">Alugar livro</span>
+
                                 </a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="../list/list_costumers.php">
-                                    <span data-feather="file-text" class="align-text-bottom"></span>
-                                    Listagem
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="../edit/edit_users.php">
-                                    <span data-feather="file-text" class="align-text-bottom"></span>
-                                    Editar itens
+                                    <span data-feather="edit_rentals" class="align-text-bottom">Listagem</span>
+
                                 </a>
                             </li>
                         </ul>
@@ -191,16 +181,34 @@ if (($result_authors) and ($result_authors->rowCount() != 0)) {
                                     </div>
                                     <div>
                                         <?php
-                                        $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
 
-                                        if (!empty($dados["edit_authors"])) {
-                                            var_dump($dados);
+                                        if (isset($_POST['edit_authors'])) {
+                                            $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+
+                                            // Valide os dados aqui, se necessário
+
+                                            $query_update = "UPDATE authors SET name = :name WHERE id = :id";
+                                            $stmt_update = $pdo->prepare($query_update);
+                                            $name = trim($dados['name']); // Apply filtering, removing whitespace
+                                            $stmt_update->bindValue(':name', $name);
+                                            $stmt_update->bindValue(':id', $id, PDO::PARAM_INT);
+                                            $stmt_update->execute();
+
+                                            if ($stmt_update->rowCount() > 0) {
+                                                $row_dados = $stmt_update->fetch(PDO::FETCH_ASSOC);
+                                                $name = $row_dados['name'];
+                                                $_SESSION['msg'] = "<p style='color: #090;'>Autor atualizado com sucesso!</p>";
+                                            } else {
+                                                $_SESSION['msg'] = "<p style='color: #f00;'>Erro ao atualizar o autor!</p>";
+                                            }
+
+                                            header("Location: /front/controllers/list/list_authors.php");
+                                            exit;
                                         }
-
                                         ?>
 
                                     </div>
-                                    <div class="row g-5 pt-5">
+                                    <div class="row g-5 pt-3">
                                         <div class="col-md-7 col-lg-10">
                                             <h5 class="mb-3">Informações revelantes sobre o autor</h5>
                                         </div>
@@ -208,40 +216,25 @@ if (($result_authors) and ($result_authors->rowCount() != 0)) {
 
                                     <form class="needs-validation" action="" method="post" novalidate="">
                                         <div class="row g-3">
-                                            <div class="col-sm-7">
-                                                <label for="name_author" class="form-label">Nome do autor</label>
-                                                <input type="text" class="form-control" name="name" id="name_author" value=<?php
-                                                                                                                            if (isset($dados['name'])) {
+                                            <div class="col-sm-7 ">
+                                                <label for="name_author" class="form-label mt-4">Nome do autor</label>
+                                                <input type="text" class="form-control" name="name" id="name_author" value="<?php
+                                                                                                                            if (isset($name))
                                                                                                                                 echo $dados['name'];
-                                                                                                                            } elseif (isset($row_authors['name'])) {
+                                                                                                                            elseif (isset($row_authors['name']))
                                                                                                                                 echo $row_authors['name'];
-                                                                                                                            } ?> required>
+                                                                                                                            ?>">
 
-                                                <hr class="my-4">
+
+
+                                                <hr class="my-4" </hr>
 
                                                 <input class="w-20 btn btn-primary btn-ls" type="submit" value="Salvar" name="edit_authors">
                                             </div>
                                         </div>
                                     </form>
-
-
-                                    <!-- <form class="needs-validation" action="../../../pdo/edit/edit_authors.php" method="post" novalidate>
-                                        <div class="row g-3">
-                                            <div class="col-sm-7">
-                                                <label for="name_author" class="form-label">Nome do autor</label>
-                                                <input type="text" class="form-control" name="name" id="name_author" value="" required>
-                                                <hr class="my-4">
-
-                                                <button class="w-20 btn btn-primary btn-ls" type="submit">Enviar</button>
-                                            </div>
-                                        </div>
-                                    </form> -->
                                 </div>
-                                <footer class="text-muted py-5">
-                                    <div class="container">
-                                        <p class="mb-1">© 2023 Biblioteca Pedbot</p>
-                                    </div>
-                                </footer>
+
                             </main>
                         </div>
 
@@ -249,8 +242,11 @@ if (($result_authors) and ($result_authors->rowCount() != 0)) {
 
                 </div>
             </main>
+            <footer class="text-muted text-center py-5">
 
+                <p class="mb-1">© 2023 Biblioteca Pedbot</p>
         </div>
+        </footer>
     </div>
 </body>
 
