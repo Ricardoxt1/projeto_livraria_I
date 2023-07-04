c5dcddf5ec10d18221b4e3208061beaebb86dd02
Ricardoxt1 <ricardoalexandr_@hotmail.com>
2023-07-04 15:53:12 -0300

    incremento do component para navbar menu

--- php_app/front/pages/list/listAuthors.php
+++ php_app/front/pages/list/listAuthors.php
@@ -22,17 +22,10 @@ $connection = connect();
 </head>
 
 <body>
-    <header class="navbar navbar-dark sticky-top bg-primary flex-md-nowrap p-0 shadow">
-        <a class="navbar-brand text-light col-md-3 col-lg-2 me-0 px-3 fs-6">Biblioteca Pedbot</a>
-        <button class="navbar-toggler position-center d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="navbar-nav">
-            <div class="nav-item text-nowrap">
-                <a class="nav-link px-3" href="../../menu/menu">Voltar ao menu</a>
-            </div>
-        </div>
-    </header>
+
+    <?php
+    include_once '../../componentNavbar/navbar.php';
+    ?>
 
     <div class="container-fluid">
         <div class="row">
@@ -120,7 +113,7 @@ $connection = connect();
                                             <input type='hidden' name='id' value=$id />
                                             <td name='name_authors'>$row_authors[name]</td>
 
-                                            <td name='edit_name'><a href='../edit/editAuthor.php?id=$id'><svg xmlns='http://www.w3.org/2000/svg' width='18' height='18' fill='currentColor' class='bi bi-pencil-square' viewBox='0 0 16 16'>
+                                            <td name='edit_name'><a href='../../pages/edit/editAuthor?id=$id'><svg xmlns='http://www.w3.org/2000/svg' width='18' height='18' fill='currentColor' class='bi bi-pencil-square' viewBox='0 0 16 16'>
                                                                          <path d='M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z'/>
                                                                          <path fill-rule='evenodd' d='M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z'/></svg>
                                                                  </a>
--- php_app/front/pages/list/listBooks.php
+++ php_app/front/pages/list/listBooks.php
@@ -47,7 +47,7 @@ $connection = connect();
     <main>
         <div class='album py-5 bg-light'>
             <div class='container'>
-                <div class='row row-cols-xs-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 g-3'>
+                <div class='row g-1'>
                     <?php
 
                     if (isset($_SESSION['msg'])) {
@@ -55,48 +55,76 @@ $connection = connect();
                         unset($_SESSION['msg']);
                     }
 
-                    //select para apresentar aos cards
-                    $query_books = "SELECT  books.id, books.titule, books.page, books.realese_date, books.author_id as author_id, authors.name as authors_name, publishers.name as publishers_name  FROM books INNER JOIN authors ON authors.id = books.author_id INNER JOIN publishers ON publishers.id = books.publisher_id";
-                    $result_books = $connection->prepare($query_books);
-                    $result_books->execute();
-                    if (($result_books) and ($result_books->rowCount() != 0)) {
-                        while ($row_book = $result_books->fetch(PDO::FETCH_ASSOC)) {
-                            $id = $row_book['id'];
-                            echo "<div class='col'>
-                                            <div class='card shadow-sm'>
-                                                <svg class='bd-placeholder-img card-img-top' width='100%' height='300' xmlns='http://www.w3.org/2000/svg' role='img' aria-label='Placeholder: Thumbnail' preserveAspectRatio='xMidYMid slice' focusable='false'>
-                                                    <title>Placeholder</title>
-                                                    <rect width='100%' height='100%' fill='#55595c'></rect><text x='50%' y='50%' fill='#eceeef' dy='.3em'>Thumbnail</text>
-                                                </svg>
-        
-                                                <div class='card-body p-4'>
-                                                    
-                                                    <p class='card-text'>Titulo: " . $row_book['titule'] . "</p>
-                                                    <p class='card-text'>Páginas: " . $row_book['page'] . "</p>
-                                                    <p class='card-text'>Lançamento: " . $row_book['realese_date'] . "</p>
-                                                    <p class='card-text'>Autor(a): " . $row_book["authors_name"] . "</p>
-                                                    <p class='card-text'>Editora: " . $row_book["publishers_name"] . "</p>
-                                                    <div class='d-flex justify-content-between align-items-center'>
-                                                        <div class='btn-group m-1'>
-                                                            <a class='m-2' href='../edit/editBook.php?id=$id'><svg xmlns='http://www.w3.org/2000/svg' width='22' height='22' fill='currentColor' class='bi bi-pencil-square' viewBox='0 0 16 16'>
-                                                                                                <path d='M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z'/>
-                                                                                                <path fill-rule='evenodd' d='M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z'/>
-                                                                                           </svg>
-                                                            </a>
-                                                            <a class='m-2' href='../../../controllerDB/delete/deleteBook.php?id=$id'><svg xmlns='http://www.w3.org/2000/svg' width='22' height='22' fill='currentColor' class='bi bi-trash' viewBox='0 0 16 16'>
-                                                                                                <path d='M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z' />
-                                                                                                <path d='M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z' /></svg>
-                                                             </a>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>";
-                        }
-                    } else {
-                        echo "<p style='color:red;'>Não foi possível realizar a listagem com sucesso.</p>";
-                    }
                     ?>
+                    <nav aria-label="Page navigation example ">
+                        <ul class="pagination justify-content-center">
+                            <?php
+                            // Paginação
+                            $limit = 3; // Número de itens por página
+                            $query_count = "SELECT COUNT(*) as total FROM books";
+                            $result_count = $connection->prepare($query_count);
+                            $result_count->execute();
+                            $row_count = $result_count->fetch(PDO::FETCH_ASSOC);
+                            $total_pages = ceil($row_count['total'] / $limit); // Total de páginas
+
+                            $current_page = isset($_GET['page']) ? $_GET['page'] : 1;
+                            $start = ($current_page - 1) * $limit; // Início dos registros
+
+                            $query_books = "SELECT  books.id, books.titule, books.page, books.realese_date, books.author_id as author_id, authors.name as authors_name, publishers.name as publishers_name  FROM books INNER JOIN authors ON authors.id = books.author_id INNER JOIN publishers ON publishers.id = books.publisher_id LIMIT $start, $limit";
+                            $result_books = $connection->prepare($query_books);
+                            $result_books->execute();
+
+                            while ($row_book = $result_books->fetch(PDO::FETCH_ASSOC)) {
+                                $id = $row_book['id'];
+                                echo "<div class='col'>
+                                <div class='card shadow-sm'>
+                                <svg class='bd-placeholder-img card-img-top' width='100%' height='300' xmlns='http://www.w3.org/2000/svg' role='img' aria-label='Placeholder: Thumbnail' preserveAspectRatio='xMidYMid slice' focusable='false'>
+                                <title>Placeholder</title>
+                                                <rect width='100%' height='100%' fill='#55595c'></rect><text x='53%' y='50%' fill='#eceeef' dy='.3em'>Thumbnail</text>
+                                                </svg>
+                                                
+                                                <div class='card-body p-4'>
+                                                
+                                                <p class='card-text'>Titulo: " . $row_book['titule'] . "</p>
+                                                <p class='card-text'>Páginas: " . $row_book['page'] . "</p>
+                                                <p class='card-text'>Lançamento: " . $row_book['realese_date'] . "</p>
+                                                <p class='card-text'>Autor(a): " . $row_book["authors_name"] . "</p>
+                                                <p class='card-text'>Editora: " . $row_book["publishers_name"] . "</p>
+                                                <div class='d-flex justify-content-between align-items-center'>
+                                                <div class='btn-group m-1'>
+                                                <a class='m-2' href='../edit/editBook.php?id=$id'><svg xmlns='http://www.w3.org/2000/svg' width='22' height='22' fill='currentColor' class='bi bi-pencil-square' viewBox='0 0 16 16'>
+                                                <path d='M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z'/>
+                                                <path fill-rule='evenodd' d='M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z'/>
+                                                                                       </svg>
+                                                        </a>
+                                                        <a class='m-2' href='../../../controllerDB/delete/deleteBook.php?id=$id'><svg xmlns='http://www.w3.org/2000/svg' width='22' height='22' fill='currentColor' class='bi bi-trash' viewBox='0 0 16 16'>
+                                                        <path d='M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z' />
+                                                                                            <path d='M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z' /></svg>
+                                                                                            </a>
+                                                                                            </div>
+                                                         </div>
+                                                         </div>
+                                                         </div>
+                                                         </div>";
+                            }
+
+                            // Links de paginação
+                            echo '<li class="page-item' . ($current_page == 1 ? ' disabled' : '') . '">';
+                            echo '<a class="page-link" href="?page=' . ($current_page - 1) . '" tabindex="-1">Previous</a>';
+                            echo '</li>';
+
+                            for ($i = 1; $i <= $total_pages; $i++) {
+                                echo '<li class="page-item' . ($current_page == $i ? ' active' : '') . '">';
+                                echo '<a class="page-link" href="?page=' . $i . '">' . $i . '</a>';
+                                echo '</li>';
+                            }
+
+                            echo '<li class="page-item' . ($current_page == $total_pages ? ' disabled' : '') . '">';
+                            echo '<a class="page-link" href="?page=' . ($current_page + 1) . '">Next</a>';
+                            echo '</li>';
+                            ?>
+                        </ul>
+                    </nav>
                 </div>
             </div>
         </div>
