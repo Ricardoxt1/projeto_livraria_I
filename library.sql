aac7bb7125faf49ded084c64d181497c76790b0d
Ricardoxt1 <ricardoalexandr_@hotmail.com>
2023-07-05 10:07:45 -0300

    backup

--- library.sql
+++ library.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mysql
--- Tempo de geração: 29/06/2023 às 19:30
+-- Tempo de geração: 05/07/2023 às 13:06
 -- Versão do servidor: 5.7.42
 -- Versão do PHP: 8.1.20
 
@@ -20,8 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `library`
 --
-CREATE DATABASE IF NOT EXISTS `library` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `library`;
 
 -- --------------------------------------------------------
 
@@ -33,6 +31,17 @@ CREATE TABLE `authors` (
   `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Despejando dados para a tabela `authors`
+--
+
+INSERT INTO `authors` (`id`, `name`) VALUES
+(2, 'J.R.R. Tolkien'),
+(47, 'Stephen King'),
+(48, 'Toshikazu Kawaguchi'),
+(49, 'PERIC'),
+(51, 'C.J. Tudor');
 
 -- --------------------------------------------------------
 
@@ -50,6 +59,18 @@ CREATE TABLE `books` (
   `publisher_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Despejando dados para a tabela `books`
+--
+
+INSERT INTO `books` (`id`, `titule`, `page`, `realese_date`, `author_id`, `library_id`, `publisher_id`) VALUES
+(1, 'O Hobbit', 369, '1962', 2, 1, 4),
+(5, 'As Duas Torres', 464, '1954', 2, 1, 6),
+(6, 'A Sociedade do Anel', 604, '1954', 2, 1, 3),
+(8, 'Trono de Vidro', 392, '2012', 2, 1, 20),
+(9, 'Império de Tempestades', 841, '2016', 2, 1, 20),
+(10, 'Corte de Espinhos e Rosas', 434, '2015', 2, 1, 4);
+
 -- --------------------------------------------------------
 
 --
@@ -64,6 +85,17 @@ CREATE TABLE `costumers` (
   `address` varchar(100) DEFAULT NULL,
   `email` varchar(255) NOT NULL DEFAULT 'example@example.com'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Despejando dados para a tabela `costumers`
+--
+
+INSERT INTO `costumers` (`id`, `cpf`, `name`, `phone_number`, `address`, `email`) VALUES
+(8, '89712369845', 'Fernanda F', '1499864512', 'Main ST 01', 'fernanda@gmail.com'),
+(9, '3621235972', 'Guilherme Valério', '1498631235', 'Main ST 45', 'guilherme@gmail.com'),
+(10, '8745631241', 'Hugo Silva', '14998743621', 'Main ST 02', 'hugo@gmail.com'),
+(11, '123654789412', 'Nathan M', '1499874526', 'Main ST 03', 'nathan@gmail.com'),
+(12, '59758163215', 'Rodrigo', '12334556576', 'Main ST 02', 'rodrigo@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -80,6 +112,13 @@ CREATE TABLE `employees` (
   `library_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Despejando dados para a tabela `employees`
+--
+
+INSERT INTO `employees` (`id`, `name`, `pis`, `office`, `departament`, `library_id`) VALUES
+(2, 'Gabriela', '12564894632', 'Vendedor', 'Vendas', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -91,6 +130,13 @@ CREATE TABLE `libraries` (
   `name` varchar(100) DEFAULT 'Livraria Pedbot'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Despejando dados para a tabela `libraries`
+--
+
+INSERT INTO `libraries` (`id`, `name`) VALUES
+(1, 'Livraria Pedbot');
+
 -- --------------------------------------------------------
 
 --
@@ -101,6 +147,20 @@ CREATE TABLE `publishers` (
   `id` int(11) NOT NULL,
   `name` varchar(155) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Despejando dados para a tabela `publishers`
+--
+
+INSERT INTO `publishers` (`id`, `name`) VALUES
+(3, 'Harper Collins'),
+(4, 'Rocco'),
+(6, 'Suma'),
+(8, 'Gutenberg'),
+(15, 'Formato'),
+(17, 'Valentina'),
+(18, 'Intrínseca'),
+(20, 'Galera Record');
 
 -- --------------------------------------------------------
 
@@ -116,6 +176,14 @@ CREATE TABLE `rentals` (
   `book_id` int(11) DEFAULT NULL,
   `employee_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Despejando dados para a tabela `rentals`
+--
+
+INSERT INTO `rentals` (`id`, `rental`, `delivery`, `costumer_id`, `book_id`, `employee_id`) VALUES
+(5, '2023-06-10', '2023-06-20', 9, 1, 2),
+(6, '2023-07-12', '2023-07-14', 9, 5, 2);
 
 --
 -- Índices para tabelas despejadas
@@ -180,43 +248,43 @@ ALTER TABLE `rentals`
 -- AUTO_INCREMENT de tabela `authors`
 --
 ALTER TABLE `authors`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
 
 --
 -- AUTO_INCREMENT de tabela `books`
 --
 ALTER TABLE `books`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de tabela `costumers`
 --
 ALTER TABLE `costumers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de tabela `employees`
 --
 ALTER TABLE `employees`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de tabela `libraries`
 --
 ALTER TABLE `libraries`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de tabela `publishers`
 --
 ALTER TABLE `publishers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT de tabela `rentals`
 --
 ALTER TABLE `rentals`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Restrições para tabelas despejadas
